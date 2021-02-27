f91710ed7a33f66180a5fb8482fbe366584317ee
Daniel King <chuan.e.king@gmail.com>
2021-02-26 20:45:12 -0800

    added some content to the new reminder page

--- MentalReminder/src/screens/New/index.js
+++ MentalReminder/src/screens/New/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { ImageBackground, View, Text} from 'react-native';
+import { TextInput, ScrollView, ImageBackground, View, Text} from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faBriefcase, faCoffee, faSpa, faPlus } from '@fortawesome/free-solid-svg-icons';
 import styles from './styles';
 
 
@@ -10,12 +12,55 @@ const NewScreen = (props) => {
                 source = { require('../../../assets/images/header.png')}
                 style={styles.image}
             >
-
             </ImageBackground>
 
-        <View>
-            <Text>This will be where users can make a new reminder</Text>
-        </View>
+        <ScrollView style={styles.body}>
+            <Text style={styles.title}>New reminder</Text>
+            <Text style={styles.p}>What do you want to remember?</Text>
+            <View style={styles.presetBlock}>
+                <View style={styles.preset}>
+                    <Text>
+                        <FontAwesomeIcon icon={ faCoffee } size={35} color="black" style={styles.presetIcon} />
+                        <Text style={styles.presetText}>   Drink coffee</Text>
+                    </Text>
+                </View>
+                <View style={styles.preset}>
+                    <Text>
+                        <FontAwesomeIcon icon={ faCoffee } size={35} color="black" style={styles.presetIcon} />
+                        <Text style={styles.presetText}>   Drink water</Text>
+                    </Text>
+                </View>
+                <View style={styles.preset}>
+                    <Text>
+                        <FontAwesomeIcon icon={ faBriefcase } size={35} color="black" style={styles.presetIcon} />
+                        <Text style={styles.presetText}>   HW due</Text>
+                    </Text>
+                </View>
+                <View style={styles.preset}>
+                    <Text>
+                        <FontAwesomeIcon icon={ faSpa } size={35} color="black" style={styles.presetIcon} />
+                        <Text style={styles.presetText}>   Meditate</Text>
+                    </Text>
+                </View>
+                <View style={styles.customPreset}>
+                    <Text>
+                        <FontAwesomeIcon icon={ faPlus } size={35} color="black" style={styles.presetIcon} />
+                    </Text>
+                    <TextInput
+                        {...props}
+                        editable
+                        maxLength={30}
+                        style={{ marginLeft: 17, paddingLeft: 5, height: 30, borderColor: 'gray', borderWidth: 1, width: 230 }}
+                        value="Custom preset"
+                    />
+                </View>
+            </View>
+
+            <View style={styles.time}>
+                <Text style={styles.p}>When do you want to be reminded?</Text>
+                
+            </View>
+        </ScrollView>
 
         </View>
     );
--- MentalReminder/src/screens/New/styles.js
+++ MentalReminder/src/screens/New/styles.js
@@ -10,6 +10,53 @@ const styles = StyleSheet.create({
         resizeMode: 'contain',
     },
 
+    body: { 
+        paddingTop: 50,
+        paddingLeft: 37
+    },
+
+    title: { 
+        fontSize: 40,
+        fontWeight: 'bold',
+        paddingBottom: 30,
+    },
+
+    preset: { 
+        paddingTop: 7,
+        paddingBottom: 7,
+        paddingLeft: 15
+    },
+
+    presetIcon: { 
+        width: 30,
+    },
+
+    presetText: { 
+        fontSize: 19
+    },
+    presetBlock: { 
+        paddingTop: 20,
+        paddingBottom: 20
+    },
+
+    customPreset: { 
+        flex: 1,
+        flexDirection: 'row', 
+        alignItems: 'center',
+        paddingTop: 7,
+        paddingBottom: 7,
+        paddingLeft: 15
+    },
+
+    time: { 
+        marginTop: 30
+    },
+
+
+    p: { 
+        fontSize: 19,
+    }
+
 
 });
 export default styles;
